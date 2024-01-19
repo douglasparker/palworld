f1995e0220b6344334fb7ac2b28ad67d14aac104
Douglas Parker <me@douglasparker.dev>
2024-01-19 13:27:31 +0000

    fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,9 @@
 #tail -f --retry "${ARK_PATH}/Saved/Logs/ShooterGame.log" &
 
 # Start the server
+eval /home/steam/Steam/steamapps/common/PalServer/PalServer.sh &
 #eval $SERVER_CMD > /dev/null 2>&1 &
-#SERVER_PID=$!
 
-#wait $SERVER_PID
+SERVER_PID=$!
+wait $SERVER_PID
 exit $?
